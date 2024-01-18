6a3af9d264528dbdc37e4e3ffa95be8937374a09
Karna-Balaji-07 <beast141ghost07@gmail.com>
2024-01-18 15:14:37 +0530

    First commitss

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,6 @@ public class Hello {
     public static void main(String args[]){
         System.out.println("hello world");
         System.out.println("Welcome to the world of JAVA!");
+        System.out.print(" This is awesome jobs!1!");
     }
 }
