7d173a35fb7b733303f93d73524c55bea8bbd22c
Karna-Balaji-07 <beast141ghost07@gmail.com>
2024-01-19 16:24:40 +0530

    Basics of Java.

---
+++ src/Loops.java
@@ -0,0 +1,30 @@
+public class Loops {
+    public static void main(String args[]){
+        System.out.println("Loops!!");
+
+
+        System.out.println("For loops!!");
+        for(int i=1;i<=5;i++){
+            System.out.println(i*i);
+        }
+
+
+        System.out.println(" ");
+        System.out.println("While loops ");
+        int j = 1;
+        while(j<=10){
+            System.out.println("Test case "+j);
+            j++;
+        }
+
+        System.out.println(" ");
+        System.out.println(" Do while loops");
+        int k=5;
+        do{
+            System.out.println("test case "+k);
+            k++;
+        }while(k>=10);
+
+    }
+
+}
